b4d091cd4cae9f66702cf60d476caf9795b5198d
AudreiPavanello <audreipavanello@hotmail.com>
2024-10-30 23:07:14 -0300

    Add new features
    Add modular structures
    
    Add new tabs
    
    Add dictionairies

---
+++ global.R
@@ -0,0 +1,12 @@
+library(shiny)
+library(bslib)
+library(microdatasus)
+library(dplyr)
+library(openxlsx)
+library(shinyjs)
+library(waiter)
+library(DT)
+
+# Source all helper files
+source("R/constants.R")
+source("R/dictionaries.R")
